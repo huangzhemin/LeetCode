89c74dd9ada0b53b9dcce386ac1463b492e81b75
huangzhemin <huang_zhe_min@163.com>
2015-08-04 20:48:35 +0800

    脑子总是短路，注意力在哪

---
+++ 121 Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+ typedef struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ } TreeNode;
+
+ typedef struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ }ListNode;
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+
+class Solution {
+public:
+    //Say you have an array for which the ith element is the price of a given stock on day i.=
+    //If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+    //思路：利润最大就是卖出价-买入价最大，而卖出价一定是在时间上后于买入价，想一想只用在O(n)时间复杂度下就能完成。
+    //买入价肯定是在前面，所以直接记录最低价格，和后面的数字到与前面最小的差价。
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.size(); i ++) {
+            if (prices[i] < minPrice) minPrice = prices[i];
+            else {
+                int tempProfit = prices[i] - minPrice;
+                if (tempProfit > maxProfit) maxProfit = tempProfit;
+            }
+        }
+        return maxProfit;
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
---
+++ 122 Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+ typedef struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ } TreeNode;
+
+ typedef struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ }ListNode;
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+
+class Solution {
+public:
+    //Say you have an array for which the ith element is the price of a given stock on day i.
+    //Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times).
+    //However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+    //思路：区别与1，允许多次交易，每次交易必须是先买再卖，而且同一时间只能持有一手。这下就有意思了，如何才能达到最大的收益率？
+    //第一次扫描的时候记录波峰，何为波峰，前后的值都比这个值小，创立一个数组记录波峰点，在波峰点前面的波谷点买入，计算总的最大收益。
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty() || prices.size() == 1) return 0;
+        if (prices.size() == 2) {
+            if (prices[1] > prices[0]) return prices[1] - prices[0];
+            else return 0;
+        }
+
+        int maxProfit = 0;
+        //找波峰
+        int *maxPrices = new int[prices.size()];
+        for (int i = 0; i < prices.size(); i ++) {
+            maxPrices[i] = 0;
+        }
+        //开始找波峰
+        for (int i = 0; i < prices.size() - 2; i ++) {
+            if (prices[i + 1] > prices[i] && prices[i + 1] >= prices[i + 2]) maxPrices[i + 1] = 1;
+        }
+
+//        for (int i = 0; i < prices.size(); i ++) {
+//            cout << maxPrices[i] << endl;
+//        }
+        //当maxPrices为1时，计算前面为0的最小值
+        int startIndex = 0;
+        int endIndex = 0;
+        int tempMaxPrice;
+        int minPrice = INT_MAX;
+
+        for (int i = 0; i < prices.size(); i ++) {
+            if (maxPrices[i] == 1) {
+                endIndex = i - 1;
+                tempMaxPrice = prices[i] - minPrice;
+                maxProfit += tempMaxPrice;
+                startIndex = i;
+                endIndex = i;
+                minPrice = INT_MAX;
+            } else {
+                if (prices[i] < minPrice) minPrice = prices[i];
+            }
+        }
+        int lastProfit = prices[prices.size() - 1] - minPrice;
+        if (lastProfit > 0) maxProfit += lastProfit;
+
+        delete []maxPrices;
+        return maxProfit;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> price;
+    price.push_back(1);
+    price.push_back(2);
+    price.push_back(4);
+//    price.push_back(2);
+//    price.push_back(6);
+//    price.push_back(6);
+//    price.push_back(2);
+//    price.push_back(9);
+//    price.push_back(1);
+//    price.push_back(0);
+//    price.push_back(7);
+//    price.push_back(4);
+//    price.push_back(5);
+//    price.push_back(0);
+
+    cout << endl << s.maxProfit(price) << endl;
+    return 0;
+}
